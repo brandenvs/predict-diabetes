45c6becfdb4d07c70298c38c00803a93afd3a15b
Branden van Staden <92578986+BrandenSysoutHelloWorld@users.noreply.github.com>
2023-11-06 19:08:35 +0200

    Improved Accuracy Dramatically with Preprocessing

--- myNetworkPrototypes/network_prototype.v3/Network.v3(stable)/latest__network.prototype.v3.0.py
+++ myNetworkPrototypes/network_prototype.v3/Network.v3(stable)/latest__network.prototype.v3.0.py
@@ -1,10 +1,11 @@
+# IMPORTS
 import pandas as panda
 import numpy as np
 import tensorflow as tf
 from sklearn.preprocessing import LabelEncoder
 from ydata_profiling import ProfileReport
 
-# Constants
+# CONSTANTS
 SHUFFLE_BUFFER = 500
 BATCH_SIZE = 32
 
@@ -15,58 +16,75 @@ def create_dframe(csv_path=str()):
     return dframe
 
 # Function Usage: Preprocessing of Data Frame
-def preprocess_data(dframe=panda.DataFrame()):
-    # Handle missing values
-    dframe = dframe.fillna(method='ffill')  # Forward-fill missing values
+def tensor_slicer(dframe=panda.DataFrame(), target=panda.Series()):
+    # NOTE TRAINING MODEL & TENSOR_FLOW DATASET SLICING(optimization)
+    # Define Features of Dataset after Label has been dropped 
+    features = dframe
 
-    # Encode categorical columns
-    encoder = LabelEncoder()
-    encoded_features = [feat for feat in dframe.columns if dframe[feat].dtypes == 'object']
-    dframe[encoded_features] = dframe[encoded_features].apply(encoder.fit_transform)
+    # Create a new dataset which contains Tensor slices
+    sliced_dframe = tf.data.Dataset.from_tensor_slices((features.values, target.values))
+    sliced_dframe = sliced_dframe.shuffle(SHUFFLE_BUFFER).batch(BATCH_SIZE)
 
-    return dframe
+    return sliced_dframe
+
+# NOTE Uncertain if this is the correct func. name. 
+# Function Usage: This Function makes a copy of Data Frames feature, Define parameters for the Diabetes Model and Compiles the Diabetes Model.
+def preprocessing_model(dframe=panda.DataFrame()):
+    # Make copy of dframe
+    features = dframe.copy()
+    
+    # Setup diabetes model
+    diabetes_model = tf.keras.Sequential([
+        tf.keras.layers.Input(shape=(features.shape[1],)), # Adjusts the model's shape based on Data Frame Features
+        tf.keras.layers.Dense(10, activation='relu'),
+        tf.keras.layers.Dense(10, activation='relu'),
+        tf.keras.layers.Dense(1)
+    ])
+    # Compile diabetes model
+    diabetes_model.compile(optimizer='adam',
+                loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+                metrics=['accuracy'])
+    return diabetes_model
 
 # Function Usage: Normalize Data Frame
 def normalize_dframe(dframe=panda.DataFrame()):
+    # Make a copy of the data frame
     normalized_dframe = dframe.copy()
-    # 
-    normalized_dframe = normalized_dframe.replace("?", np.NaN)    
 
-    # 
-    print(normalized_dframe.isnull().sum())
+    print('\n-------------------\nBEFORE NORMALIZATION LAYER:\n')
+    normalized_dframe.info()
+    print('Once you have inspected the data frame before normalization layer has been applied PRESS [ENTER] TO CONTINUE...')
+    input()
 
-    # BESPOKE TO 10 YEAR DIABETES DATASET - BASED ON RELATIONS WITH NON-NULL ENTRIES
-    # [START]
-    normalized_dframe = normalized_dframe.where(~normalized_dframe.race.isnull(),normalized_dframe.fillna(axis=0,method='ffill')) # 
-    #
-    normalized_dframe = normalized_dframe.where(~normalized_dframe.payer_code.isnull(),normalized_dframe.fillna(axis=0,method='ffill')) # 
-    #
-    normalized_dframe = normalized_dframe.where(~normalized_dframe.medical_specialty.isnull(),normalized_dframe.fillna(axis=0,method='ffill')) # 
-    #
-    normalized_dframe = normalized_dframe.where(~normalized_dframe.diag_2.isnull(),normalized_dframe.fillna(axis=0,method='ffill')) # 
-    #
-    normalized_dframe = normalized_dframe.where(~normalized_dframe.diag_3.isnull(),normalized_dframe.fillna(axis=0,method='ffill')) # 
-    #
+    # Handle missing values
+    normalized_dframe = normalized_dframe.fillna(method='ffill')  # Forward-fill missing values
+
+    normalized_dframe = normalized_dframe.where(~normalized_dframe.race.isnull(),normalized_dframe.fillna(axis=0,method='ffill'))
+    # #
+    normalized_dframe = normalized_dframe.where(~normalized_dframe.payer_code.isnull(),normalized_dframe.fillna(axis=0,method='ffill')) 
+    # #
+    normalized_dframe = normalized_dframe.where(~normalized_dframe.medical_specialty.isnull(),normalized_dframe.fillna(axis=0,method='ffill'))
+    # #
+    normalized_dframe = normalized_dframe.where(~normalized_dframe.diag_2.isnull(),normalized_dframe.fillna(axis=0,method='ffill'))
+    # #
+    normalized_dframe = normalized_dframe.where(~normalized_dframe.diag_3.isnull(),normalized_dframe.fillna(axis=0,method='ffill'))
+
+    # Handle Missing entires for weight with the mode value
     normalized_dframe['weight'] = normalized_dframe['weight'].fillna(normalized_dframe['weight'].mode()[0]) # 
-    #
+    # Handle Ranges for age entires
     normalized_dframe['age'] = normalized_dframe.age.str.extract('(\d+)-(\d+)').astype('int').mean(axis=1).astype('int') # 
-    # [END]
 
-    # Inspect the newly ADJUSTED DataFrame fields(first 4 rows)
-    # print('\n----------------------------------------------\nDataFrame Head\n(normalized_dframe initial and BEFORE passing to normalization layer)\n')
-    # print(normalized_dframe.head())
-
-    #  normalized_dframe = normalized_dframe.replace(None, np.NaN)
-
+    # Encoding Data Frame features
     encoder = LabelEncoder()
     encoded_features = [feat for feat in normalized_dframe.columns if normalized_dframe[feat].dtypes == 'object']
-
     normalized_dframe[encoded_features] = normalized_dframe[encoded_features].apply(encoder.fit_transform)
     
-    # 
-    print(f'\n----------------------------------------------\nTotal Count of NULL values in Data Frame.\n>>>Before Passing to Normalization Layer')
-    print(normalized_dframe.isnull().sum())
-    print(normalized_dframe.head())    
+    print('\n-------------------\nAFTER NORMALIZATION LAYER:\n')
+    normalized_dframe.info()
+    print('Once you have inspected the data frame AFTER normalization layer has been applied PRESS [ENTER] TO CONTINUE...')
+    input()
+
+    # Return the normalized DATA FRAME
     return normalized_dframe
 
 # Function Usage: Generate Data Statistics & Render HTML Overview of Data Frame
@@ -97,49 +115,39 @@ def overview_data(dframe=panda.DataFrame()):
     jupyter_report.to_file(output_file='myNetworkPrototypes\\reports\\diabetes_report.html')
     return jupyter_report
 
+
+# Terminal Menu
+def menu():
+    pass
+
 # NOTE DATA FRAME SETUP & NORMALIZATION LAYER
 # Get Data Frame
 _dframe = create_dframe('myNetworkPrototypes/datasets/diabetes.csv')
 # Normalize Data Frame
-normal_dframe = normalize_dframe(_dframe)
+_dframe = normalize_dframe(_dframe)
 
 # NOTE DATA FRAME GENERATE REPORT & PREPROCESSING MODEL
 # Generate Data Frame Report
-######################### report = overview_data(_dframe) REMOVE hashtags! REMOVE hashtags! REMOVE hashtags! 
-# Preprocess Data Frame
-_dframe = preprocess_data(_dframe)
+# report = overview_data(_dframe)
 
-# NOTE TRAINING MODEL & TENSORFLOW DATASET SLICING(optimization)
-# Define Label which model will train on
-target = _dframe.pop('weight')
-# Define Features of Dataset after Label has been dropped 
-features = _dframe
 
-# Create a new dataset which contains Tensor slices
-sliced_dataset = tf.data.Dataset.from_tensor_slices((features.values, target.values))
-sliced_dataset = sliced_dataset.shuffle(SHUFFLE_BUFFER).batch(BATCH_SIZE)
+# TESTING THE MODEL #
+# Define Target Series
+_target = _dframe.pop('weight')
+# Pass Normalized Data Frame & Target Series to Tensor Sliced Objects 
+_sliced_dframe = tensor_slicer(_dframe, _target)
 
-# Setup diabetes model
-diabetes_model = tf.keras.Sequential([
-    tf.keras.layers.Input(shape=(features.shape[1],)), # Adjusts the model's shape
-    tf.keras.layers.Dense(10, activation='relu'),
-    tf.keras.layers.Dense(10, activation='relu'),
-    tf.keras.layers.Dense(1)
-])
-
-# Compile diabetes model
-diabetes_model.compile(optimizer='adam',
-              loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
-              metrics=['accuracy'])
+# Get the Diabetes Model
+diabetes_model = preprocessing_model(_dframe)
 
 # Train diabetes model 
 # NOTE  I use the slices for an effective highly efficient approach(consider the size of our dataset. 
 #       This could be the only way to local train!)
-diabetes_model.fit(sliced_dataset, epochs=10)
+diabetes_model.fit(_sliced_dframe, epochs=3)
 
 # Save diabetes model to project folder(myNetworkPrototypes\network_prototype.v3\model)
 try:
     diabetes_model.save('myNetworkPrototypes\\network_prototype.v3\\model\\(trained)diabetes_model.v3')
-    print('MODEL SUCCESSFULLY TRAINED & IS SAVED TO NETWORKS DIRECTORY(myNetworkPrototypes\\network_prototype.v3\\model\\diabetes_model.v3))')
+    print('MODEL SUCCESSFULLY TRAINED & SAVED TO NETWORKS DIRECTORY\n(myNetworkPrototypes\\network_prototype.v3\\model\\diabetes_model.v3))')
 except:
     print('error: MODEL WAS UNABLE TO SAVE')
